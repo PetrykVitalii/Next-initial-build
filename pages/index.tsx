8a8c69172db8a89fa81f945f2eab5d3a4fa5ea3a
Vitalii <petruk.v.m2@gmail.com>
2022-07-15 16:23:09 +0300

    eslint rule

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,12 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
 
 interface Props {}
 
-const Home: NextPage<Props> = () => {
-  console.log('a');
-
-  // const [films, setFilms] = useState<IFilm[]>([]);
-
-  // useEffect(() => {
-  //   if (filmsState === RequestState.LOADED) {
-  //     return;
-  //   }
-
-  //   if (initialFilms) {
-  //     dispatch(filmsActions.setFilmsState(RequestState.LOADED));
-  //     dispatch(filmsActions.setFilms(initialFilms));
-  //     return;
-  //   }
-
-  //   dispatch(getFilms() as unknown as AnyAction);
-  // }, [initialFilms]);
-
-  // useEffect(() => {
-  //   setFilms(selectedFilms);
-  // }, [selectedFilms]);
-
-  return (
-    <div>Main</div>
-  );
-};
-
-export async function getServerSideProps() {
-  return { props: { initialFilms: [] } };
-}
+const Home: NextPage<Props> = () => (
+  <Link href="/faq">
+    <a>FAQ</a>
+  </Link>
+);
 
 export default Home;
